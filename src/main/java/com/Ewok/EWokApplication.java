3644672891c44591856d9b86a896d4447cb46288
Luis Hernando Soto Mesa <59453252+LUINFO89@users.noreply.github.com>
2023-10-03 17:11:22 -0500

    ESTRUCTURA

---
+++ src/main/java/com/Ewok/EWokApplication.java
@@ -0,0 +1,13 @@
+package com.Ewok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EWokApplication.class, args);
+	}
+
+}
