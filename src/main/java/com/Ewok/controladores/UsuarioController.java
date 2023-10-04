ac63940996e7f5ffdb908e57c9e1dbab3366b72a
Luis Hernando Soto Mesa <59453252+LUINFO89@users.noreply.github.com>
2023-10-04 16:46:25 -0500

    UsuarioControllerSaveandRepositoryUsuario

--- src/main/java/com/Ewok/controladores/UsuarioController.java
+++ src/main/java/com/Ewok/controladores/UsuarioController.java
@@ -1,4 +1,21 @@
 package com.Ewok.controladores;
 
+import com.Ewok.modelos.Usuario;
+import com.Ewok.repositorios.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/usuarios")
+
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    private Object MediaType;
+
+    @PostMapping(value = "/agregar")
+    public Usuario agregarUsuario(@RequestBody Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 }
--- src/main/java/com/Ewok/repositorios/UsuarioRepository.java
+++ src/main/java/com/Ewok/repositorios/UsuarioRepository.java
@@ -1,4 +1,10 @@
 package com.Ewok.repositorios;
+import com.Ewok.modelos.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public class UsuarioRepository {
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+    // Spring Data JPA implementa automáticamente los métodos CRUD
 }
+
